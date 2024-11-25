b5e5bc2794d3d7c7520058a41c207bcdbf41d9b5
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-11-25 10:20:13 -0500

    added literal solution

--- lib/regex.py
+++ lib/regex.py
@@ -1,6 +1,6 @@
 import re
 
-my_pattern = r"[A-z]*\'*[A-z]\s[A-z' ]*today'*[a-z, ]*[.\?]+"
+my_pattern = r"It's such a lovely day today.|Some weather we're having today\, huh\?|Maybe today's just not my day."
 my_regex = re.compile(my_pattern)
 print(my_regex)
 # ([A-z]\'[A-z]\.)|([A-z]\'[A-z]\,[A-z]\?)
