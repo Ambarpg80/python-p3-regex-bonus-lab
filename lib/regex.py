a7943e892e24d9a8a5ac912a62d216394b042f68
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-11-25 10:21:57 -0500

    with previous solution in comments

--- lib/regex.py
+++ lib/regex.py
@@ -3,5 +3,5 @@ import re
 my_pattern = r"It's such a lovely day today.|Some weather we're having today\, huh\?|Maybe today's just not my day."
 my_regex = re.compile(my_pattern)
 print(my_regex)
-# ([A-z]\'[A-z]\.)|([A-z]\'[A-z]\,[A-z]\?)
+#"[A-Z][a-z]*'*[a-z]\s[a-z' ]*today'*[a-z, ]*[?\.]+" (solution)
 #"It's such a lovely day today.|Some weather we're having today\, huh\?|Maybe today's just not my day."
